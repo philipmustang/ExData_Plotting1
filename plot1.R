f0c10b430397bf787868aa648ad9c391eb753e4c
Ai Thanh Ho <atho@adviso.ca>
2017-12-11 00:16:19 -0500

    Submit Project

---
+++ plot1.R
@@ -0,0 +1,14 @@
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table(unz(temp, "household_power_consumption.txt"),header = TRUE, sep = ";",na.strings = "?")
+unlink(temp)
+data$Time<-strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+df<-subset(data,Date==as.Date("2007-02-01")|Date==as.Date("2007-02-02"))
+png("plot1.png", width=480, height=480)
+hist(df$Global_active_power,
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     col="red")
+dev.off()
